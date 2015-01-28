Interior evaluation problems fixed by flipping the sign convention for tractions. This is slightly confusing as the sign convention should not matter as long as it is consistent.
Beam bending very close to working properly. The remaining problem seems
to be effective handling of the no terms, just constant RHS constraints.

tools/beam_bend.py
@@ -22,7 +22,7 @@ G_fic = G_from_E_mu(E_fic, mu_fic)
 
 input_filename = 'test_data/beam_bend.in'
 
-def solution(x, y):
+def disp_bc(x, y):
     ux = (-P * x ** 2 * y) / (2 * E_fic * I) \
           - (mu_fic * P * y ** 3) / (6 * E_fic * I) \
           + (P * y ** 3) / (6 * I * G_fic) \
@@ -36,14 +36,18 @@ def solution(x, y):
 
     return ux, uy
 
+def upper_lower_trac_bc(x, y):
+    return np.zeros_like(x), np.zeros_like(x)
+
 def plotter():
-    nx = 15
-    ny = 15
+    nx = 20
+    ny = 20
     x_vals = np.linspace(0.0, L, nx)
     y_vals = np.linspace(-c, c, ny)
+    y_vals = [-c, c]
     x, y = np.meshgrid(x_vals, y_vals)
 
-    ux, uy = solution(x, y)
+    ux, uy = disp_bc(x, y)
 
     plt.figure()
     plt.contourf(x, y, ux)
@@ -53,6 +57,7 @@ def plotter():
     plt.contour(x, y, uy, linestyles = 'solid', colors = 'k', linewidths = 2)
     plt.figure()
     plt.quiver(x, y, ux, uy)
+    plt.ylim([-1.1,1.1])
     plt.show()
 
 def create_file():
@@ -61,12 +66,12 @@ def create_file():
     # left_edge = Element([[0, -c], [0, c]], "traction", [[0, P], [0, P]], refine)
 
     es = []
-    es.extend(line([[0, c], [0, -c]], refine, solution))
-    es.extend(line([[0, -c], [L, -c]], refine, solution))
-    # es.append(Element([[0, -c], [L, -c]], "traction", [[0, 0], [0, 0]], refine))
-    es.extend(line([[L, -c], [L, c]], refine, solution))
-    es.extend(line([[L, c], [0, c]], refine, solution))
-    # es.append(Element([[L, c], [0, c]], "traction", [[0, 0], [0, 0]], refine))
+    es.extend(line([[0, c], [0, -c]], refine, "displacement", disp_bc))
+    # es.extend(line([[0, -c], [L, -c]], refine, "displacement", disp_bc))
+    es.append(Element([[0, -c], [L, -c]], "traction", [[0, 0], [0, 0]], refine))
+    es.extend(line([[L, -c], [L, c]], refine, "displacement", disp_bc))
+    # es.extend(line([[L, c], [0, c]], refine, "displacement", disp_bc))
+    es.append(Element([[L, c], [0, c]], "traction", [[0, 0], [0, 0]], refine))
 
     # fictitious_mu = mu / (1 - mu)
     # fictitious_E = E / (1 - mu ** 2)
@@ -75,10 +80,21 @@ def create_file():
     exec_template(input_filename, es = es, G = G, mu = mu)
     print("Input file created")
 
-if __name__ == "__main__":
-    # plotter()
+def test_all_displacements():
     create_file()
     run_file(input_filename)
-    filename = 'test_data/beam_bend.disp_outint'
-    check_field(filename, solution, False, 2)
+    # trac_filename = 'test_data/beam_bend.trac_out'
+    # check_field(trac_filename, upper_lower_trac_bc, False, -5)
+    disp_filename = 'test_data/beam_bend.disp_out'
+    check_field(disp_filename, disp_bc, False, 7)
+    disp_intfilename = 'test_data/beam_bend.disp_outint'
+    check_field(disp_intfilename, disp_bc, False, 7)
 
+# def test_upper_lower_traction_free():
+#     create_file()
+#     run_file(input_filename)
+#     filename = 'test_data/beam_bend.disp_outint'
+#     check_field(filename, disp_bc, False, 2)
+
+if __name__ == "__main__":
+    plotter()

tools/input_builder.py
@@ -77,10 +77,8 @@ def exec_template(filename, **params):
     with open(filename, 'w') as file:
         file.write(text)
 
-def run_file(filename, suppress_out):
+def run_file(filename, stdout_dest = None):
     popen_params = dict(shell = True)
-    if suppress_out:
-        popen_params['stdout'] = subprocess.PIPE
     process = subprocess.Popen('./run ' + filename, **popen_params)
     process.wait()
 
@@ -108,6 +106,7 @@ def check_field(filename, solution, plot_diff, digits):
     #TODO: It would be better to use norms here
     diffx = np.abs(exactx - datax)
     diffy = np.abs(exacty - datay)
+    print exactx,datax,diffx
     if plot_diff:
         plt.figure()
         plt.quiver(x, y, datax, datay)

tools/pressured_cylinder.py
@@ -19,7 +19,7 @@ def build_trac_bc(a, b, p_a, p_b, E, mu):
     def trac_bc(x, y):
         theta = np.arctan2(y, x)
         r = np.sqrt(x ** 2 + y ** 2)
-        pressure = np.where(r < ((a + b) / 2), -p_a, p_b)
+        pressure = np.where(r < ((a + b) / 2), p_a, -p_b)
         p_x = pressure * np.cos(theta)
         p_y = pressure * np.sin(theta)
         return p_x, p_y
@@ -88,12 +88,12 @@ def pressured_cylinder(bc_types):
     delete_files(input_filename)
     create_file(a, b, E, mu, input_filename,
                 bc_types, bc_funcs)
-    run_file(input_filename, False)
+    run_file(input_filename)
     if 'displacement' in bc_types.values():
         check_field(traction_filename, trac_bc, False, -5)
     if 'traction' in bc_types.values():
         check_field(displacement_filename, disp_bc, False, 7)
-    # check_field(interior_disp_filename, disp_bc, False, 7)
+    check_field(interior_disp_filename, disp_bc, False, 7)
 
 def test_trac_trac():
     pressured_cylinder(dict(inner = "traction", outer = "traction"))