Gravity works. Fixed stress sign convention.

acceptance_tests/test_crack.py
@@ -10,7 +10,7 @@ def build_slip_bc(a, stress_drop, G, nu):
         # Classic Griffith crack solution. Normally, the solution is given
         # as a displacement for each crack face. I multiply by 2 to get the
         # slip across the crack.
-        slip = 2 * (1 - nu) * (stress_drop / G) * np.sqrt(a ** 2 - pt[0] ** 2)
+        slip = -2 * (1 - nu) * (stress_drop / G) * np.sqrt(a ** 2 - pt[0] ** 2)
         return slip, np.zeros_like(pt[0])
     return slip_bc
 

acceptance_tests/test_gravity.py
@@ -5,15 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def test_gravity():
-    r = 1000.0
+    r = 1.0
     es = []
     es.extend(circle([0, 0], r, 6, "displacement", lambda x: np.zeros_like(x), False))
     es.extend(circle([0, 0], r, 6, "gravity", lambda x: np.zeros_like(x), False))
 
     params = dict(
-        obs_order = 5,
-        singular_steps = 12,
-        near_tol = 1e-6,
         shear_modulus = 30e9,
         poisson_ratio = 0.25,
         gravity = True,
@@ -24,50 +21,25 @@ def test_gravity():
 
     pts = []
     x = y = np.linspace(-r, r, 100)
-    x_mat, y_mat = np.meshgrid(x, y)
-    pts = np.array([x_mat.flatten(), y_mat.flatten()]).T
+    for i in range(x.shape[0]):
+        for j in range(y.shape[0]):
+            if x[i] ** 2 + y[j] ** 2 < r ** 2:
+                pts.append([x[i], y[j]])
+    pts = np.array(pts)
     normalsx = np.array([[1, 0]] * pts.shape[0])
     normalsy = np.array([[0, 1]] * pts.shape[0])
 
     ux, uy = result.interior_displacement(pts)
     sxx, sxy = result.interior_traction(pts, normalsx)
     sxy, syy = result.interior_traction(pts, normalsy)
-
-    mask = x_mat ** 2 + y_mat ** 2 > r ** 2
-    sxy_mat = sxy.reshape(x_mat.shape)
-    sxy_mat[mask] = np.nan
-    x_mat[mask] = np.nan
-    y_mat[mask] = np.nan
-    plt.contourf(x_mat, y_mat, sxy_mat,
-        levels = np.linspace(-8e7, 8e7, 25), extend = 'both')
-    plt.xlim([-r, r])
-    plt.ylim([-r, r])
-    plt.show()
-
-    import sys; sys.exit()
-
-    r = np.sqrt(pts[:, 0] ** 2 + pts[:, 1] ** 2)
-    plt.figure()
-    plt.plot(r, uy, '.')
-    plt.title('uy')
-    plt.figure()
-    plt.plot(pts[:, 1], sxx, '.')
-    plt.title('sxx')
-    plt.figure()
-    plt.plot(pts[:, 0], sxy, '.')
-    plt.title('sxy')
-    plt.figure()
-    plt.plot(pts[:, 1], syy, '.')
-    plt.title('syy')
-    plt.show()
-    # plt.figure()
-    # plt.quiver(pts[:, 0], pts[:, 1], ux, uy)
-
-    # plt.figure()
-    # plt.quiver(pts[:, 0], pts[:, 1], sxx, sxy)
-    # plt.figure()
-    # plt.quiver(pts[:, 0], pts[:, 1], sxy, syy)
-    # plt.show()
+    assert(np.allclose(np.min(uy), -1.10e-7, atol = 1e-9))
+    assert(np.allclose(np.max(uy), 0.0, atol = 1e-9))
+    assert(np.allclose(np.min(syy), -19500, atol = 50.0))
+    assert(np.allclose(np.max(syy), 19500, atol = 50.0))
+    assert(np.allclose(np.min(sxy), -6500, atol = 50.0))
+    assert(np.allclose(np.max(sxy), 6500, atol = 50.0))
+    assert(np.allclose(np.min(sxx), -6500, atol = 50.0))
+    assert(np.allclose(np.max(sxx), 6500, atol = 50.0))
 
 if __name__ == '__main__':
     test_gravity()

acceptance_tests/test_lame_problems.py
@@ -31,7 +31,7 @@ build_disp_bc[3] = build_disp_bc3d
 def build_trac_bc2d(a, b, p_a, p_b, E, mu):
     def trac_bc(pt):
         r, theta = circ_from_cart(*pt)
-        pressure = np.where(r < ((a + b) / 2), p_a, -p_b)
+        pressure = np.where(r < ((a + b) / 2), -p_a, p_b)
         return cart_from_circ(pressure, theta)
     return trac_bc
 
@@ -41,7 +41,7 @@ def build_trac_bc3d(a, b, p_a, p_b, E, mu):
         term1 = (p_a * a ** 3 - p_b * b ** 3) / (b ** 3 - a ** 3)
         term2 = ((p_a - p_b) * b ** 3 * a ** 3) / ((b ** 3 - a ** 3) * r ** 3)
         sigmarr = term1 - term2
-        sigmarr = np.where(r > ((a + b) / 2.0), -sigmarr, sigmarr)
+        sigmarr = np.where(r < ((a + b) / 2.0), -sigmarr, sigmarr)
         return cart_from_sph(sigmarr, theta, phi)
     return trac_bc
 

elastic/solver.py
@@ -42,11 +42,6 @@ class Result(object):
             )
 
             # The result is negated to move it to the other side of the equation
-            print(term)
-            try:
-                print(f[0][0])
-            except Exception:
-                pass
             result -= op.apply(np.concatenate(f)) * term['multiplier']
         out = []
         for d in range(self.tbem.dim):
@@ -124,13 +119,11 @@ def dense_solver(tbem, input, dof_map, constraint_matrix, systems):
             row_scaling = scalings[bie['obs_mesh']]
             col_scaling = scalings[op['spec']['src_mesh']]
             reshaped_op *= row_scaling * col_scaling
+            reshaped_op *= op['spec']['multiplier']
             matrix[start_row:end_row, start_col:end_col] = reshaped_op
     matrix = tbem.DenseOperator(n, n, matrix.reshape(n * n))
     condensed_op = tbem.condense_matrix(constraint_matrix, constraint_matrix, matrix)
     right_hand_sides = [s['rhs'] for s in systems]
-    # TODO: Figure out why this negation is necessary for the dense solver but
-    # not the iterative solver!
-    right_hand_sides[0] = -right_hand_sides[0]
     scale_rows(right_hand_sides, input.bies, input.params)
     rhs = concatenate_condense(tbem, dof_map, constraint_matrix, right_hand_sides)
     np_op = condensed_op.data().reshape((rhs.shape[0], rhs.shape[0]))